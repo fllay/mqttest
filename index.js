683cccecebaf0b03c6dd078454f173df7ad17791
fllay <calgalli@gmail.com>
2022-05-27 09:56:56 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const aedes = require("aedes")();
 const httpServer = require("http").createServer();
 const ws = require("websocket-stream");
-const port = 80;
+const port = 1883;
 
 ws.createServer({ server: httpServer }, aedes.handle);
 
